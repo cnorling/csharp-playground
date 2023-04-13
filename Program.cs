dcbd9a3326fc9f5a93d9df13706b0a898c1f81b9
cerise norling <salineselin@gmail.com>
2023-04-12 23:32:42 -0600

    adding some content

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Console.WriteLine("Hello, World!");
+
+public class Car
+{
+  // properties
+  public string Make { get; set; }
+  public string Model { get; set; }
+  public int Year { get; set; }
+
+  // constructor
+  public Car(string make, string model, int year)
+  {
+    Make = make;
+    Model = model;
+    Year = year;
+  }
+
+  // methods
+  public void PrintInfo()
+  {
+    Console.WriteLine(this.Make);
+    Console.WriteLine(this.Model);
+    Console.WriteLine(this.Year);
+  }
+}
