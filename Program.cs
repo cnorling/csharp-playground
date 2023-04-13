99889543fdaf44b50ac779151b8634478678b044
cerise norling <salineselin@gmail.com>
2023-04-12 23:41:01 -0600

    added namespace

--- Program.cs
+++ Program.cs
@@ -2,29 +2,32 @@
 
 using System;
 
-Car vehicle = new Car("Toyota", "Corolla", 1998);
+Automobiles.Car vehicle = new Automobiles.Car("Toyota", "Corolla", 1998);
 vehicle.PrintInfo();
 
-public class Car
+namespace Automobiles
 {
-  // properties
-  public string Make { get; set; }
-  public string Model { get; set; }
-  public int Year { get; set; }
-
-  // constructor
-  public Car(string make, string model, int year)
+  public class Car
   {
-    Make = make;
-    Model = model;
-    Year = year;
-  }
+    // properties
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
 
-  // methods
-  public void PrintInfo()
-  {
-    Console.WriteLine(this.Make);
-    Console.WriteLine(this.Model);
-    Console.WriteLine(this.Year);
+    // constructor
+    public Car(string make, string model, int year)
+    {
+      Make = make;
+      Model = model;
+      Year = year;
+    }
+
+    // methods
+    public void PrintInfo()
+    {
+      Console.WriteLine(this.Make);
+      Console.WriteLine(this.Model);
+      Console.WriteLine(this.Year);
+    }
   }
 }
